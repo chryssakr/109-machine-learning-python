1b9c06ef2960ed3f3409b645296e7ad0aba6eedb
Chryssa Karatza <chryssakaratza@gmail.com>
2024-12-29 15:18:40 +0000

    add code from class1

---
+++ class1.py
@@ -0,0 +1,120 @@
+# print("Hello Class!")
+
+# # variables
+# age = 25
+# height = 1.75
+# name = "John"
+
+# print(f"Name: {name}\nAge: {age}\nHeight: {height}")
+
+# # basic operations
+# a = 5
+# b = 3
+# print("a + b =", a + b)
+# print("a - b =", a - b)
+# print("a * b =", a * b)
+# print("a / b =", a / b)
+# print("a % b =", a % b)
+
+# # Assignment
+
+# result_addition = age + 5
+# result_subtraction = height - 0.5
+# result_multiplication = age * 2
+# result_division = height / 2
+# result_modulus = age % 7
+
+# print("\nBasic Operations:")
+# print("Age after adding 5 years:", result_addition)
+# print("Height after subtracting 0.5:", result_subtraction)
+# print("Double the age:", result_multiplication)
+# print("Half the height:", result_division)
+# print("Age modulo 7:", result_modulus)
+
+# # Integers
+# integer_var = 10
+
+# # Floats
+# float_var = 3.14
+
+# # Strings
+# string_var = "Hello, Python!"
+
+# # Booleans
+# boolean_var = True
+
+# # Lists (mutable)
+# list_var = [1, 2, 3, "four", 5.0]
+
+# # Tuples (immutable)
+# tuple_var = (10, "tuple", 3.14)
+
+# # Sets
+# set_var = {1, 2, 3, 3, 4, 5}  # Note: Sets automatically remove duplicate elements
+
+# # Displaying values
+# print("Integer:", integer_var)
+# print("Float:", float_var)
+# print("String:", string_var)
+# print("Boolean:", boolean_var)
+# print("List:", list_var)
+# print("Tuple:", tuple_var)
+# print("Set:", set_var)
+
+# # Show the type of the data
+
+# a = 5.0
+# print(type(a))
+
+# b = "This is a string"
+# print(type(b))
+
+# # Data conversion
+
+# # Integer to float
+# int_value = 42
+# float_value = float(int_value)
+# print("Integer to Float:", float_value)
+
+# # Float to integer
+# float_number = 3.14
+# int_number = int(float_number)
+# print("Float to Integer:", int_number)
+
+# # Integer to string
+# num = 123
+# str_num = str(num)
+# print("Integer to String:", str_num)
+
+# # String to integer
+# str_value = "456"
+# int_value_from_str = int(str_value)
+# print("String to Integer:", int_value_from_str)
+
+# # Other operations
+
+# # Exponentiation
+# x = 2
+# x = x**3
+# print("Exponentiation:", x)
+
+# # Floor division
+# x = x // 3
+# print("Floor division:", x)  # We only get the integer part. Instead of 2.66666
+
+# # Assignment operators
+# x = 7
+
+# print("Before", x)
+# x += 3  # Same as x = x + 3
+# print("After", x, "\n")
+
+# print("Before", x)
+# x *= 2  # Same as x = x * 2
+# print("After", x, "\n")
+
+# # Operations can be performed considering that the data are of the same type. For example:
+# print(16.15 - float("10"))
+
+# # However, strings can not be turned to numerics if they contain characters
+# float("24-10-2024")
